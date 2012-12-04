5cc78520973427c11eba1782b0b9455068f6b9e5
mackwai <mackenzie.cumings@gmail.com>
2012-12-04 09:17:11 -0500

    calculate confidence for testimonial cost metric
    changed code to calculate confidences for testimonial cost metric; only
    small datasets are used because the use of Fractions instead of floats
    makes the search much slower.

--- calculate_confidences.py
+++ calculate_confidences.py
@@ -1,5 +1,7 @@
 #!/cygdrive/c/Python27/python.exe
+import fractions
 import os
+
 from units import *
 
 
@@ -7,7 +9,13 @@ if __name__ == '__main__':
   for file_name in os.listdir( '../data/tournaments' ):
     #print path
     path = os.path.join( '../data/tournaments', file_name )
-    prior = calculate_prior_probability_of_true_judgement(get_lines_from_file(path))
-    print prior
-    calculate_testimonial_cost = close_testimonial_cost(0.51)
-    print "%s %f" % ( file_name, estimate_confidence(path,calculate_testimonial_cost,10) )
+    #prior = calculate_prior_probability_of_true_judgement(get_lines_from_file(path))
+    #print prior
+    
+    if os.path.getsize(path) > 5000:
+      continue
+    
+    calculate_testimonial_cost = close_testimonial_cost(fractions.Fraction(11,20))
+    print "%s %f" % ( file_name, estimate_confidence_for_probability_function(path,calculate_testimonial_cost,100) )
+    
+    #print "%s %f" % ( file_name, estimate_confidence_for_bojar(path,1000) )
